fd74e6babd9ce91aea743367dfa98df55aeca459
AJ Speller <ajspeller@gmail.com>
2017-07-07 15:40:49 -0400

    Set onclick handler for the button and created the method

--- app/src/main/java/com/ajspeller/beeradvisor/FindBeerActivity.java
+++ app/src/main/java/com/ajspeller/beeradvisor/FindBeerActivity.java
@@ -2,6 +2,9 @@ package com.ajspeller.beeradvisor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends Activity {
 
@@ -9,5 +12,15 @@ public class FindBeerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    // call when the user click the button
+    public void onClickFindBeer(View v) {
+
+        TextView brands = findViewById(R.id.brands);
+        Spinner color = findViewById(R.id.color);
+        String beerType = (color.getSelectedItem()).toString();
+        brands.setText(beerType);
+
     }
 }
